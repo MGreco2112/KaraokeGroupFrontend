1ba10fc6596b069b57fa21a1ec8746ebff1b666b
Matthew Greco <mgreco2112@tutanota.com>
2023-05-24 12:36:05 -0400

    removed auto deletion system as error was being thrown in backend. Refactoring to be button activated in the room page. Created get call to backend for room id param, creating button logic to leave room and delete guest

--- src/components/room/Room.js
+++ src/components/room/Room.js
@@ -3,13 +3,19 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { apiHostUrl, loginToken} from "../config";
 import { AuthContext } from "../providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import Button from "../common/Button";
 import Container from "../common/Container";
 
 const Room = () => {
     const params = useParams();
 
+    const navigate = useNavigate();
+
+    const [auth, setAuth, saveAuth] = useContext(AuthContext);
+
     const [room, setRoom] = useState({
-        id: params.id
+        id: null
     });
 
     const [loading, setLoading] = useState(true);
@@ -32,13 +38,24 @@ const Room = () => {
         }
     }
 
+    const _leaveRoom = async () => {
+        try {
+            
+        } catch (err) {
+            console.error(err.response ? err.response : err.message);
+        }
+    }
+
     _getRoom();
     console.log(room);
+
+
 
     
 
     return(
         <Container style={{minHeight: '0em'}}>
+            <Button>Leave Room</Button>
         </Container>
     );
 }
