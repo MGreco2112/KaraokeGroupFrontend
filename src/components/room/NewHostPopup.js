abb2acfbe8239309ea9a9a512c51c17e26b961d0
Matthew Greco <mgreco2112@tutanota.com>
2023-06-04 00:03:07 -0400

    began work on NewHostPopup component

---
+++ src/components/room/NewHostPopup.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from "../common/Container";
+
+const NewHostPopup = (props) => {
+    const {guests, prompt} = props.guests;
+
+    let positon = 1;
+
+    //return floating component containing the prompt, a map of guests, and an input field
+    return(
+        <Container style={{minHeight: '0em'}}>
+            <h1>NewHostPopup</h1>
+        </Container>
+    );
+}
+
+export default NewHostPopup;
