909a9c592c048f10f6d1f717c21f5eb86a156d09
Matthew Greco <mgreco2112@tutanota.com>
2023-07-19 12:31:05 -0400

    began work on button component

--- src/components/room/Song.js
+++ src/components/room/Song.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Container from "../common/Container";
+import BorderCard from "../common/BorderCard";
+import InlineInputContainer from "../common/InlineInputContainer";
+import Button from "../common/Button";
 
 const Song = (props) => {
 
     const {id, name, spotifySongURL} = props;
 
+    const onClick = () => {
+        //call backend to delete song from room
+    }
+
     return(
         <Container style={{minHeight: '0em'}}>
-            <p>{name}</p>
+            <BorderCard>
+                <InlineInputContainer>
+                    <p>{name}</p>
+                    <Button>Remove</Button>
+                </InlineInputContainer>
+            </BorderCard>
         </Container>
     );
 }
